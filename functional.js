d073086f590534cf5983c4a0ce060210ad55f1e1
ljnissen <lj_nissen@gmx.de>
2018-04-29 16:08:27 +0000

    Add functional.js which splits words of an array and lowercase as well as adding hyphen

--- functional.js
+++ functional.js
@@ -1,1 +1,11 @@
 var states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"];
+
+// map: Imperative version
+function imperativeMap(states) {
+	var urlStates = [];
+	states.forEach(function(state)  {
+		urlStates.push(state.toLowerCase().split(/\s+/).join("-"));
+	});
+	return urlStates;
+}
+console.log(imperativeMap(states));
