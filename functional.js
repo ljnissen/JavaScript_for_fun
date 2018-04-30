a97050ebf47d352da7d167834bfef2a8b6adef26
ljnissen <lj_nissen@gmx.de>
2018-04-30 16:27:13 +0000

    Add a functional version of map

--- functional.js
+++ functional.js
@@ -9,3 +9,11 @@ function imperativeMap(states) {
 	return urlStates;
 }
 console.log(imperativeMap(states));
+
+// map: Functional version
+function functionalMap(states) {
+	return states.map(function(state)  {return state.toLowerCase().split(/\s+/).join('-')});
+}
+console.log(functionalMap(states));
+
+[1, 2, 3, 4, 5].map(function(n) { return n * n; });
