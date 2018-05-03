a4d3f8c8c586f6f0094fe7d4fa0253991895492c
ljnissen <lj_nissen@gmx.de>
2018-05-03 16:32:51 +0000

    Add array slicing

---
+++ array_slicing.js
@@ -0,0 +1,32 @@
+a = [42, 8, 17, 99];
+
+b = a.slice(1, 3);
+
+c = a.length;
+
+d = a[a.length - 1];
+
+e = a.slice(-1)[0]
+
+console.log(b);
+
+console.log(c);
+
+console.log(d);
+
+console.log(e);
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+y = x.slice(2,8);
+
+z = x[x.length - 8];
+
+j = x[x.length - 7];
+
+console.log(y);
+
+console.log(z);
+
+console.log(j);
+
