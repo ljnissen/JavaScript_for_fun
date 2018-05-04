caf6184f2eb64ef9dc01808e17d20a45ffa7429f
ljnissen <lj_nissen@gmx.de>
2018-05-04 16:39:40 +0000

    Add code for array methods

--- array_slicing.js
+++ array_slicing.js
@@ -1,32 +1,64 @@
-a = [42, 8, 17, 99];
+b = [42, 8, 17, 99, 55];
 
-b = a.slice(1, 3);
+//b = a.slice(1, 3);
 
-c = a.length;
+//c = a.length;
 
-d = a[a.length - 1];
+//d = a[a.length - 1];
 
-e = a.slice(-1)[0]
+//e = a.slice(-1)[0]
 
-console.log(b);
+//f = a.sort();
 
-console.log(c);
+//g = a.reverse();
+
+//h = a.push(6);
+
+//i = a.push("kuken");
+
+//j = a.pop("kuken");
+
+//console.log(b);
+
+//console.log(c);
+
+//console.log(d);
+
+//console.log(e);
+
+//console.log(f);
+
+//console.log(g);
+
+//console.log(h);
+
+//console.log(i);
+
+//console.log(a);
+
+//console.log(j);
+
+
+//x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//y = x.slice(2,8);
+
+//z = x[x.length - 8];
+
+//j = x[x.length - 7];
+
+//console.log(y);
+
+//console.log(z);
+
+//console.log(j);
+
+//var lastElement = a.pop();
+//console.log(lastElement);
+
+a = ["ant", "bat", "cat", 42];
+b = a.join();
+c = a.join(", ");
+d = a.join(" == ");
 
 console.log(d);
-
-console.log(e);
-
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-y = x.slice(2,8);
-
-z = x[x.length - 8];
-
-j = x[x.length - 7];
-
-console.log(y);
-
-console.log(z);
-
-console.log(j);
-
