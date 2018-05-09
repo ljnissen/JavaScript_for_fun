1b0c2519af1fe2c13b3154222a143559752c4720
ljnissen <lj_nissen@gmx.de>
2018-05-09 16:55:17 +0000

    Add math functions

---
+++ math_and_number.js
@@ -0,0 +1,36 @@
+Math.log10 = Math.log10 || function(x) {
+  return Math.log(x) * Math.LOG10E;
+};
+
+a = 1 + 1;
+console.log(a);
+
+b = 2 - 3;
+console.log(b);
+
+c = 3 * 3;
+console.log(c);
+
+d = 2 / 3;
+console.log(d);
+
+e = Math.PI;
+console.log(e);
+
+f = Math.pow(2, 3);
+console.log(f);
+
+g = Math.sqrt(3);
+console.log(g);
+
+h = Math.cos(2*Math.PI);
+console.log(h);
+
+i = Math.log(Math.E);
+console.log(i);
+
+j = Math.log(10);
+console.log(j);
+
+k = Math.log10(Math.E);
+console.log(k);
